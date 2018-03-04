be2a97509361e0c4c2316cdcd211e501176f0388
olavoie <olavoie9507@gmail.com>
2018-03-03 23:23:12 -0500

    ...

--- src/proc_control/Mode/PositionMode.cc
+++ src/proc_control/Mode/PositionMode.cc
@@ -238,13 +238,13 @@ namespace proc_control {
 
         local_error[ROLL] = 0.0;
         local_error[PITCH] = 0.0;
-        if (fabs(orientation[YAW] - world_orientation_[2]) > 180) {
-            local_error[YAW] = fmod(orientation[YAW] + (360 - world_orientation_[2]), 360.0);
+        if (fabs(orientation[2] - world_orientation_[2]) > 180) {
+            local_error[YAW] = fmod(orientation[2] + (360 - world_orientation_[2]), 360.0);
             if (local_error[YAW] > 180) {
                 local_error[YAW] -= 360;
             }
         } else {
-            local_error[YAW] = orientation[YAW] - world_orientation_[2];
+            local_error[YAW] = orientation[2] - world_orientation_[2];
         }
 
         return local_error;
